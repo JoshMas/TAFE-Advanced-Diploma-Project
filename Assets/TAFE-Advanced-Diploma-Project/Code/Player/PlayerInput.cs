3ae5eadcb9d2b0090a589d852d29aac59f76586b
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-10-26 17:21:00 +1100

    Basic player movement tests
    Unity's new input system is pretty nice

---
+++ Assets/TAFE-Advanced-Diploma-Project/Code/Player/PlayerInput.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 1;
+    private float walkValue;
+
+    [SerializeField] private float jumpStrength = 1;
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rigid.velocity = new Vector2(walkValue * walkSpeed, rigid.velocity.y);
+    }
+
+    private void OnJump()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, jumpStrength);
+    }
+
+    private void OnCrouch(InputValue _value)
+    {
+        transform.localScale = new Vector3(1, _value.isPressed ? .5f : 1, 1);
+    }
+
+    private void OnWalk(InputValue _value)
+    {
+        walkValue = _value.Get<float>();
+    }
+}
