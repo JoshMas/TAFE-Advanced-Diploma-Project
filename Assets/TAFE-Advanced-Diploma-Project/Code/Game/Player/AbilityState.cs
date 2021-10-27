54b566f5a17bb16bdbd0e37176a572d2632ed9a8
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-10-28 00:41:18 +1100

    Update AbilityState.cs

--- Assets/TAFE-Advanced-Diploma-Project/Code/Game/Player/AbilityState.cs
+++ Assets/TAFE-Advanced-Diploma-Project/Code/Game/Player/AbilityState.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AbilityState
+public abstract class AbilityState : ScriptableObject
 {
     private Player player;
 
-    protected virtual void OnEnter() { }
+    protected virtual void OnEnter(Player _player) { player = _player; }
     protected virtual void OnUpdate() { }
     protected virtual void OnExit() { }
 }
