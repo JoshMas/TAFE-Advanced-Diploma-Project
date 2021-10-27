d6926ad9d20edd9ec5fba0344925df109ad6b001
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-10-27 13:19:14 +1100

    Added an energy bar

---
+++ Assets/TAFE-Advanced-Diploma-Project/Code/Player/EnergyBar.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class EnergyBar
+{
+    [SerializeField] private float maxEnergy = 100;
+    [SerializeField] private float rateOfGain = 25;
+    [SerializeField] private Image energyBarImage;
+    private float barWidth;
+    private float barheight;
+    private float currentEnergy = 0;
+
+    public void Initialise()
+    {
+        barWidth = energyBarImage.rectTransform.rect.width;
+        barheight = energyBarImage.rectTransform.rect.height;
+        UpdateEnergyBar();
+    }
+
+    public bool HasEnergy()
+    {
+        return currentEnergy > 0;
+    }
+
+    public void Charge()
+    {
+        currentEnergy += rateOfGain * Time.deltaTime;
+        UpdateEnergyBar();
+    }
+
+    public void Drain(float _rateOfDrain)
+    {
+        currentEnergy -= _rateOfDrain * Time.deltaTime;
+        UpdateEnergyBar();
+    }
+
+    public void Spend(float _energyCost)
+    {
+        currentEnergy -= _energyCost;
+        UpdateEnergyBar();
+    }
+
+    private void UpdateEnergyBar()
+    {
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+        float fraction = currentEnergy / maxEnergy;
+        energyBarImage.rectTransform.sizeDelta = new Vector2(fraction * barWidth, barheight);
+    }
+}
--- Assets/TAFE-Advanced-Diploma-Project/Code/Player/Player.cs
+++ Assets/TAFE-Advanced-Diploma-Project/Code/Player/Player.cs
@@ -12,14 +12,23 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpStrength = 1;
     Rigidbody2D rigid;
 
+    
+    [SerializeField] private EnergyBar energy;
+    private bool isCharging = false;
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        energy.Initialise();
     }
 
     private void Update()
     {
         rigid.velocity = new Vector2(walkValue * walkSpeed, rigid.velocity.y);
+        if (isCharging)
+        {
+            energy.Charge();
+        }
     }
 
     private void OnJump()
@@ -30,6 +39,8 @@ public class Player : MonoBehaviour
     private void OnCrouch(InputValue _value)
     {
         transform.localScale = new Vector3(1, _value.isPressed ? .5f : 1, 1);
+        isCharging = _value.isPressed;
+        walkSpeed *= _value.isPressed ? .5f : 2;
     }
 
     private void OnWalk(InputValue _value)
