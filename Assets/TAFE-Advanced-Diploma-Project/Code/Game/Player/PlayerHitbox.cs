0314a21a0ead7cc7245e103b432c1d888ae63ce9
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-11-15 12:35:52 +1100

    Files rearranged
    Also, started work on projectiles

--- Assets/TAFE-Advanced-Diploma-Project/Code/Game/Player/PlayerHitbox.cs
+++ Assets/TAFE-Advanced-Diploma-Project/Code/Game/Player/PlayerHitbox.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class PlayerHitbox : MonoBehaviour
 {
+    private float damage;
+    public float Damage
+    {
+        get
+        {
+            return damage;
+        }
+        set
+        {
+            damage = value;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
---
+++ Assets/TAFE-Advanced-Diploma-Project/Code/Game/Player/Projectiles/BasicProjectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicProjectile : MonoBehaviour
+{
+
+    [SerializeField] protected float speed = 1;
+    [SerializeField] protected float duration = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
